0a98b349cb3e4c6c8fd40da975c4314e0c852fa8
Aripal <dloerari@gmail.com>
2016-07-31 15:48:11 +0200

    first commit

---
+++ scripts/app.js
@@ -0,0 +1,15 @@
+// The role here is to kick of the application (module), starting place.
+angular
+.module("ngClassifieds", ["ngMaterial"])
+.config(function($mdThemingProvider){
+
+  $mdThemingProvider.theme('default')
+    .primaryPalette('teal')
+    .accentPalette('orange');
+
+})
+.directive("helloWorld", function() {
+  return {
+    template: "<h1> {{ message }}</h1>"
+  }
+})
