d56e35714909b65d7ce11a017ec0831fb42bb7e1
Ari <dleroari@gmail.com>
2016-08-09 19:04:05 +0200

    Add save function

--- components/classifieds.ctr.js
+++ components/classifieds.ctr.js
@@ -3,7 +3,7 @@
 
   angular
       .module("ngClassifieds") // It gives us a reference of the declared module in app.js
-      .controller("classifiedsCtrl", function($scope, $http, classifiedsFactory) {
+      .controller("classifiedsCtrl", function($scope, $http, classifiedsFactory, $mdSidenav) {
 
         /* http methods:
         1. POST - Sending data
@@ -14,5 +14,24 @@
         classifiedsFactory.getClassifieds().then(function(data){
             $scope.classifieds = data.data; // We have some "ReturnedData" data that is being returned, and we need to access the data property thats on it.
         });
+        // Open sidenav, connected to the button in toolbar
+        $scope.openSidebar = function(){
+          $mdSidenav('left').open();
+        }
+        // Closes the sidenav, connected to cancel button
+        $scope.closeSidebar = function() {
+          $mdSidenav('left').close();
+        }
+
+        $scope.saveClassified = function(classified){
+
+          // Add if statement to prevent data being stored wrong-manner.
+          if (classified) {
+              $scope.classifieds.push(classified);
+              $scope.classified = {}; // Reset form
+              $scope.closeSidebar();
+          }
+        }
+
       });
 })();
