f1ad6fc3d44107fbc6c055b5a41a17b15e53a6e3
Ari <dleroari@gmail.com>
2016-08-10 21:45:51 +0200

    Made CRUD of static JSON

--- components/classifieds.ctr.js
+++ components/classifieds.ctr.js
@@ -3,7 +3,7 @@
 
   angular
       .module("ngClassifieds") // It gives us a reference of the declared module in app.js
-      .controller("classifiedsCtrl", function($scope, $http, classifiedsFactory, $mdSidenav) {
+      .controller("classifiedsCtrl", function($scope, $http, classifiedsFactory, $mdSidenav, $mdToast, $mdDialog) {
 
         /* http methods:
         1. POST - Sending data
@@ -11,9 +11,16 @@
         3. PUT - Edite data
         4. DELETE - Delete data*/
         /*$http.get('data/classifieds.json')*/
-        classifiedsFactory.getClassifieds().then(function(data){
-            $scope.classifieds = data.data; // We have some "ReturnedData" data that is being returned, and we need to access the data property thats on it.
+        classifiedsFactory.getClassifieds().then(function(classifieds){
+            $scope.classifieds = classifieds.data; // We have some "ReturnedData" data that is being returned, and we need to access the data property thats on it.
         });
+
+        var contact = {
+          name: "Anderson Silva",
+          phone: "(444) 400-740",
+          email: "dleroari@gmail.com"
+        }
+
         // Open sidenav, connected to the button in toolbar
         $scope.openSidebar = function(){
           $mdSidenav('left').open();
@@ -25,12 +32,52 @@
 
         $scope.saveClassified = function(classified){
 
-          // Add if statement to prevent data being stored wrong-manner.
+          // Add if statement to prevent data being stored in wrong-manner.
           if (classified) {
+              classified.contact = contact; // Pretending to be info from user profile
               $scope.classifieds.push(classified);
               $scope.classified = {}; // Reset form
               $scope.closeSidebar();
+              showToast("Classified Saved!");
           }
+        }
+
+        $scope.editClassified = function(classified){
+          $scope.editing = true;
+          $scope.openSidebar();
+          $scope.classified = classified;
+        }
+
+        $scope.saveEdit = function(){
+          $scope.editing = false;
+          $scope.classified = {};
+          $scope.closeSidebar();
+          showToast("Edit Saved!");
+        }
+
+        $scope.deleteData = function(event, data){
+          var confirm = $mdDialog.confirm()
+          .title("Are you sure you want to delete item: " + data.title + "?")
+          .textContent("Item description " + data.description)
+          .targetEvent(event)
+          .ok("Yes")
+          .cancel("No");
+          // The show method returns a promise, we add the rest of the code with the "then" method.
+          $mdDialog.show(confirm).then(function() {
+            var index = $scope.classifieds.indexOf(data); // Finding the index of classified
+            $scope.classifieds.splice(index, 1);
+          }, function() {
+            // Cancel is runned here.
+          });
+        }
+
+        function showToast(message){
+          $mdToast.show(
+            $mdToast.simple()
+            .content(message)
+            .position('top, right')
+            .hideDelay(3000)
+          );
         }
 
       });
